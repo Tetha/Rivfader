5bd9496fa598b763d8f5c9e99c5ede434b362506
Harald Kraemer <harryk@freenet.de>
2009-11-08 14:41:30 +0100

    violations fixing

--- src/main/java/edu/rivfader/commands/DropTable.java
+++ src/main/java/edu/rivfader/commands/DropTable.java
@@ -2,10 +2,21 @@ package edu.rivfader.commands;
 
 import edu.rivfader.data.Database;
 
+/**
+ * This is the implementation of the drop table query.
+ * @author harald.
+ */
 public class DropTable implements ICommand {
+    /**
+     * contains the name of the table to drop.
+     */
     private String tableToDrop;
 
-    public DropTable(String tableName) {
+    /**
+     * constructs a new DropTable command.
+     * @param tableName the name of the table to drop.
+     */
+    public DropTable(final String tableName) {
         tableToDrop = tableName;
     }
 
--- src/main/java/edu/rivfader/commands/ICommand.java
+++ src/main/java/edu/rivfader/commands/ICommand.java
@@ -2,6 +2,24 @@ package edu.rivfader.commands;
 
 import edu.rivfader.data.Database;
 
+/**
+ * This is the interface for all toplevel commands.
+ *
+ * A toplevel command is basically a way of representing a
+ * certain type of query, for example, there is a toplevel
+ * command to drop a table, to update a table or to query 
+ * a table (which is the print toplevel command).
+ * This design was chosen, as it solves the problems
+ * 'how to write in relational algebra?' and 
+ * 'why does a select query print things on the console?'
+ * in a clean way: They are just toplevel commands which
+ * are parametrized with relational algebra expressions.
+ * @author harald
+ */
 public interface ICommand {
+    /**
+     * this is called to execute the toplevel command.
+     * @param context the database to evaluate the command in
+     */
     void execute(Database context);
 }
