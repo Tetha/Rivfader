502f6850c0c5bff344668043b597808789948e6d
Harald Kraemer <harryk@freenet.de>
2009-12-13 15:39:59 -0500

    Implemented visitor basis

---
+++ src/main/java/edu/rivfader/relalg/BaseRelalgTransformation.java
@@ -0,0 +1,30 @@
+package edu.rivfader.relalg;
+
+public abstract class BaseRelalgTransformation<R>
+    implements IRelAlgExprTransformation<R> {
+    @Override
+    public R transform(IRelAlgExpr input) {
+        R o; // output
+        if (input instanceof Product) {
+            o = transformProduct((Product) input);
+        } else if (input instanceof Projection) {
+            o = transformProjection((Projection) input);
+        } else if (input instanceof Selection) {
+            o = transformSelection((Selection) input);
+        } else if (input instanceof LoadTable) {
+            o = transformLoadTable((LoadTable) input);
+        } else if (input instanceof RenameTable) {
+            o = transformRenameTable((RenameTable) input);
+        } else {
+            throw new IllegalArgumentException("Unexpected IRelAlgExpr "
+                                                + input.toString());
+        }
+        return o;
+    }
+
+    public abstract R transformProduct(Product p);
+    public abstract R transformProjection(Projection p);
+    public abstract R transformSelection(Selection s);
+    public abstract R transformLoadTable(LoadTable l);
+    public abstract R transformRenameTable(RenameTable r);
+}
