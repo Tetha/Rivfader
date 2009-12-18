fd53f9305413fc0a7ff51451bc629d38b46440a0
Harald Kraemer <harryk@freenet.de>
2009-12-18 17:11:05 -0500

    Fixed bug: costs of nodes counted multiple times.

--- src/main/java/edu/rivfader/profiling/Block1Costs.java
+++ src/main/java/edu/rivfader/profiling/Block1Costs.java
@@ -3,12 +3,17 @@ package edu.rivfader.profiling;
 import edu.rivfader.relalg.Selection;
 import edu.rivfader.relalg.Projection;
 import edu.rivfader.relalg.Product;
+import edu.rivfader.relalg.IRelAlgExpr;
 
+import java.util.Set;
+import java.util.HashSet;
 public class Block1Costs implements ICostAccumulator {
     private int costs;
+    private Set<IRelAlgExpr> countedNodes;
 
     public Block1Costs() {
         costs = 0;
+        countedNodes = new HashSet<IRelAlgExpr>();
     }
 
     @Override
@@ -16,21 +21,39 @@ public class Block1Costs implements ICostAccumulator {
                                    int inputRows,
                                    int outputRows,
                                    int columns) {
-        costs += outputRows * columns;
+        if (countedNodes.contains(actingNode)) {
+            return;
+        } else {
+            System.err.println(actingNode.toString() + " " + inputRows + " " + columns);
+            costs += outputRows * columns;
+            countedNodes.add(actingNode);
+        }
     }
 
     @Override
     public void handleProjectionStatistics(Projection actingNode,
                                     int inputRows,
                                     int columns) {
-        costs += actingNode.getSelectedFields().size() * inputRows;
+        if (countedNodes.contains(actingNode)) {
+            return;
+        } else {
+            System.err.println(actingNode.toString() + " " + inputRows + " " + columns);
+            costs += actingNode.getSelectedFields().size() * inputRows;
+            countedNodes.add(actingNode);
+        }
     }
 
     @Override
     public void handleProductStatistics(Product actingNode,
                                  int rows,
                                  int columns) {
-        costs += rows * columns;
+        if (countedNodes.contains(actingNode)) {
+            return;
+        } else {
+            System.err.println(actingNode.toString() + " " + rows + " " + columns);
+            costs += rows * columns;
+            countedNodes.add(actingNode);
+        }
     }
 
     @Override
