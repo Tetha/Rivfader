df493406ebd13b497c4f2306f967b90a86b79119
Harald Kraemer <harryk@freenet.de>
2009-12-18 01:38:53 -0500

    Added new class: Block1Costs for implementing the specified code metrics
    Detailed additions:
     - Test: selectionCost = #selectedRows * #columns is red
     - Test: projectioncosts = #projections * #rows is red
     - Test productCosts = #leftrows * #rightrows * #columns red
     - Test costs = sum of all statistics-costs

---
+++ src/main/java/edu/rivfader/profiling/Block1Costs.java
@@ -0,0 +1,41 @@
+package edu.rivfader.profiling;
+
+import edu.rivfader.relalg.Selection;
+import edu.rivfader.relalg.Projection;
+import edu.rivfader.relalg.Product;
+
+public class Block1Costs implements ICostAccumulator {
+    private int costs;
+
+    public Block1Costs() {
+        costs = 0;
+    }
+
+    @Override
+    public void handleSelectionStatistics(Selection actingNode,
+                                   int inputRows,
+                                   int outputRows,
+                                   int columns) {
+        costs += outputRows * columns;
+    }
+
+    @Override
+    public void handleProjectionStatistics(Projection actingNode,
+                                    int inputRows,
+                                    int columns) {
+        costs += actingNode.getSelectedFields().size() * inputRows;
+    }
+
+    @Override
+    public void handleProductStatistics(Product actingNode,
+                                 int leftInput,
+                                 int rightInput,
+                                 int columns) {
+        costs += leftInput * rightInput * columns;
+    }
+
+    @Override
+    public int getCost() {
+        return costs;
+    }
+}
---
+++ src/test/java/edu/rivfader/test/profiling/Block1CostsTests.java
@@ -0,0 +1,98 @@
+package edu.rivfader.test.profiling;
+
+import edu.rivfader.relalg.Product;
+import edu.rivfader.relalg.Selection;
+import edu.rivfader.relalg.Projection;
+import edu.rivfader.relalg.IColumnProjection;
+import edu.rivfader.profiling.ICostAccumulator;
+import edu.rivfader.profiling.Block1Costs;
+import java.util.Collection;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import static org.junit.Assert.assertThat;
+
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.equalTo;
+
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+
+import static org.easymock.EasyMock.expect;
+import static org.powermock.api.easymock.PowerMock.createMock;
+import static org.powermock.api.easymock.PowerMock.replayAll;
+import static org.powermock.api.easymock.PowerMock.verifyAll;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({Product.class, Selection.class, Projection.class})
+public class Block1CostsTests {
+    private interface ProjectionCollection
+            extends Collection<IColumnProjection> {
+    }
+    @Test public void checkSelectionCalculation() {
+        int result;
+        Selection activeNode = createMock(Selection.class);
+        int ingoingRowCount = 10;
+        int outcomingRowCount = 5;
+        int columns = 3;
+        ICostAccumulator subject = new Block1Costs();
+        subject.handleSelectionStatistics(activeNode,
+                                          ingoingRowCount,
+                                          outcomingRowCount,
+                                          columns);
+        assertThat(subject.getCost(), is(equalTo(outcomingRowCount*columns)));
+    }
+
+    @Test public void checkProjectionCalculation() {
+        int result;
+        Projection activeNode;
+        int ingoingRows = 10;
+        int columns = 5;
+        int projectionCount = 3;
+        Collection<IColumnProjection> selectedFields;
+        ICostAccumulator subject;
+
+        activeNode = createMock(Projection.class);
+        selectedFields = createMock(ProjectionCollection.class);
+        expect(activeNode.getSelectedFields()).andReturn(selectedFields);
+        expect(selectedFields.size()).andReturn(projectionCount);
+
+        subject = new Block1Costs();
+        replayAll();
+        subject.handleProjectionStatistics(activeNode, ingoingRows, columns);
+        verifyAll();
+        assertThat(subject.getCost(), is(equalTo(projectionCount*ingoingRows)));
+    }
+
+    @Test public void checkProductCalculation() {
+        ICostAccumulator subject;
+        Product activeNode;
+        int leftRows = 10;
+        int rightRows = 100;
+        int columns = 5;
+
+        activeNode = createMock(Product.class);
+
+        subject = new Block1Costs();
+        replayAll();
+        subject.handleProductStatistics(activeNode,
+                                 leftRows, rightRows,
+                                 columns);
+        verifyAll();
+        assertThat(subject.getCost(), is(equalTo(leftRows*rightRows*columns)));
+    }
+
+    @Test public void checkCostsAddedTogether() {
+        int result;
+        Selection activeNode = createMock(Selection.class);
+        int ingoingRowCount = 10;
+        int outcomingRowCount = 5;
+        int columns = 3;
+        ICostAccumulator subject = new Block1Costs();
+        subject.handleSelectionStatistics(activeNode, ingoingRowCount,
+                                          outcomingRowCount, columns);
+        subject.handleSelectionStatistics(activeNode, ingoingRowCount,
+                                          outcomingRowCount, columns);
+        assertThat(subject.getCost(), is(equalTo(2*outcomingRowCount*columns)));
+    }
+}
